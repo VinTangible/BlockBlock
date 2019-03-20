d3fa36508cafe3376749e2aa4149c26cffc7dc55
Vincent Tang <vincenttang1994@gmail.com>
2019-03-19 22:32:57 -0700

    Created a Resource folder to user the Resource load function. Implemented spawn position. A block is spawned at the start of the game.

--- BlockBlock/Assets/Scripts/GameManager.cs
+++ BlockBlock/Assets/Scripts/GameManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    Vector2 spawnPosition;
+    GameObject[] blocks;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Currently spawn position is at 75% screen width, and 50% screen height
+        spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        spawnPosition.Scale(new Vector2((float).75, (float).5));
+        blocks = Resources.LoadAll<GameObject>("Prefabs/Blocks");
+
+        SpawnBlock();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Private Functions
+
+    private void SpawnBlock()
+    {
+        // Get a random block
+        GameObject block = blocks[Random.Range(0, blocks.Length)];
+
+        // Spawn the block at the spawn position
+        GameObject toSpawn = Instantiate(block, spawnPosition, Quaternion.identity);
     }
 }
