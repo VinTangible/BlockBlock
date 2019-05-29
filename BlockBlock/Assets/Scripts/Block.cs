bcdbe66d7f1472f531821cd7c6fec0388bd6f684
Kyle Szeto <kyl3sz3t0@gmail.com>
2019-05-28 18:15:24 -0700

    added commits

--- BlockBlock/Assets/Scripts/Block.cs
+++ BlockBlock/Assets/Scripts/Block.cs
@@ -41,6 +41,7 @@ public class Block : MonoBehaviour
             //Debug.Log("LIMIT Here");
             randomIndex = Random.Range(0, limitDegree.Length);
             GetComponent<Transform>().Rotate(0, 0, limitDegree[randomIndex]);
+            //position the block for vertical long blocks
             if(randomIndex == 1) {
                 spawnPosition = new Vector2(spawnPosition.x, spawnPosition.y);
                 GetComponent<Transform>().position = spawnPosition;
@@ -51,11 +52,14 @@ public class Block : MonoBehaviour
             //Debug.Log("Allow Here");
             randomIndex = Random.Range(0, allowDegree.Length);
             GetComponent<Transform>().Rotate(0, 0, allowDegree[randomIndex]);
+            //position the block for L shape blocks 180 an above
             if(randomIndex > 1) {
+                //for el2 blocks
                 if(el2) {
                     spawnPosition = new Vector2(spawnPosition.x, (float)(spawnPosition.y + GetBlockSize().y - 0.25));
                     GetComponent<Transform>().position = spawnPosition;
                 }
+                //for el3 blocks
                 else if(el3) {
                     spawnPosition = new Vector2(spawnPosition.x, (float)(spawnPosition.y + GetBlockSize().y + 0.5));
                     GetComponent<Transform>().position = spawnPosition;                    
@@ -64,7 +68,6 @@ public class Block : MonoBehaviour
 
         }
     }
-
     public void SetSortingLayer(string sortingLayerName, int sortingOrder) {
         foreach(Transform blockPiece in transform) {
             blockPiece.GetComponentInParent<SpriteRenderer>().sortingLayerName = sortingLayerName;
